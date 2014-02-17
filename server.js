2695d597ce134454e258979574fa30411cb5703f
MacGregor Wolstenholme <mrcybermac@gmail.com>
2014-02-17 12:30:30 -0700

    complete

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 /**
  * Created by student on 2/17/14.
  */
+var fs = require('fs');
+var http = require('http');
+http.createServer(function (req, res) {
+
+    fs.readFile('data.txt', function(err, data){
+        console.log("data", data);
+        //return data;
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(data);
+    });
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end(fileContents);
+
+    console.log("filecontents:", fileContents);
+
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
